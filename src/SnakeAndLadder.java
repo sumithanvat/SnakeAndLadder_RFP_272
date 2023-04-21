c7edd6f984f0c45ea310ece93ebf55c87ee7b993
sumithanvat <skhanvat@gmail.com>
2023-04-22 01:15:47 +0530

    Added:Check for option out of three

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -19,6 +19,28 @@ public class SnakeAndLadder {
             int roll= random.nextInt(6)+1;
             System.out.println("you rolled a "+roll);
             diceRolls++;
+
+            /*
+            UC _ 3
+             */
+            // UC 3 Check for option
+            int option = random.nextInt(3);
+            switch (option) {
+                case 0:
+                    System.out.println("Your position stays the same");
+                    break;
+                case 1:
+                    int ladder = random.nextInt(100 - playerPosition) + 1;
+                    playerPosition += ladder;
+                    System.out.println("You climbed a ladder  Your new position is " + playerPosition);
+                    break;
+                case 2:
+                    int snake = random.nextInt(playerPosition) + 1;
+                    playerPosition -= snake;
+                    System.out.println("You got swallowed by a snake Your new position is " + playerPosition);
+                    break;
+            }
+
         }
 
     }
