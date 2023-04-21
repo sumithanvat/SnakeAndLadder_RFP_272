000f899f7ef5ebcdc6adcdd6a1ab404963ed3c9d
sumithanvat <skhanvat@gmail.com>
2023-04-22 00:51:30 +0530

    Added:Starting with position 0

---
+++ src/SnakeAndLadder.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class SnakeAndLadder {
+    // main method started
+    public static void main(String[] args) {
+
+        int playerPosition = 0;
+        int diceRolls = 0;
+
+        // taking random value
+        Random random = new Random();
+
+    }
+}
