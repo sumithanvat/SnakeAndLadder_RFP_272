bd41c8e3b991656e998eef75d2a070bd005e505f
sumithanvat <skhanvat@gmail.com>
2023-04-22 01:02:38 +0530

    Added:roll number and get between 1 to 6

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -9,6 +9,17 @@ public class SnakeAndLadder {
 
         // taking random value
         Random random = new Random();
+        /*
+        UC_2
+         */
+
+        // using while loop till position is less than 100
+        while (playerPosition < 100){
+            // storing value of random to roll
+            int roll= random.nextInt(6)+1;
+            System.out.println("you rolled a "+roll);
+            diceRolls++;
+        }
 
     }
 }
